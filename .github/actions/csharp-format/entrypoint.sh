1cefe1416555fe7a55bdd42e163e9b415f324a01
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2025-01-12 04:12:59 +0100

    Add C# formatter

---
+++ .github/actions/csharp-format/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+set -o pipefail
+
+export PATH="${PATH}:/root/.dotnet/tools"
+
+LOGFILE_PATH=$(mktemp /tmp/csharpier.XXXXXX)
+cd "${INPUT_WORKING_DIRECTORY:-.}"
+dotnet-csharpier --check . 2>&1 | tee -a "$LOGFILE_PATH"
+exitcode=$?
+
+echo 'logs<<EOF' >> $GITHUB_OUTPUT
+cat $LOGFILE_PATH >> $GITHUB_OUTPUT
+echo 'EOF' >> $GITHUB_OUTPUT
+
+exit $exitcode
