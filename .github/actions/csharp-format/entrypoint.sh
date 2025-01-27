64ffed2b3f4259bc129257c0a50c63790794d5fc
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2025-01-27 15:39:00 +0100

    Move csharpier dependencies to separate file

--- .github/actions/csharp-format/entrypoint.sh
+++ .github/actions/csharp-format/entrypoint.sh
@@ -4,12 +4,12 @@ set -o pipefail
 export PATH="${PATH}:/root/.dotnet/tools"
 
 LOGFILE_PATH=$(mktemp /tmp/csharpier.XXXXXX)
-cd "${INPUT_WORKING_DIRECTORY:-.}"
+cd "${INPUT_WORKING_DIRECTORY:-.}" || exit
 dotnet-csharpier --check . 2>&1 | tee -a "$LOGFILE_PATH"
 exitcode=$?
 
-echo 'logs<<EOF' >> $GITHUB_OUTPUT
-cat $LOGFILE_PATH >> $GITHUB_OUTPUT
-echo 'EOF' >> $GITHUB_OUTPUT
+echo 'logs<<EOF' >> "$GITHUB_OUTPUT"
+cat "$LOGFILE_PATH" >> "$GITHUB_OUTPUT"
+echo 'EOF' >> "$GITHUB_OUTPUT"
 
 exit $exitcode
